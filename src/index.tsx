354553fa5ebda6751c2f2b6fef10bee789044829
Tiago Resende <tiagor87@gmail.com>
2021-09-03 19:00:07 -0300

    :art: Implements auth

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import { Home } from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="tiagor87.auth0.com"
+      clientId="3BjtW0YixYPQ2OE02GH4m0x0LQbia1nD"
+      redirectUri="http://localhost:3000">
+        <Home />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from "../components/Button";
 
 export const Home: React.FC = () => {
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+    const button = !isAuthenticated
+        ? <Button text="Entrar" onClick={loginWithRedirect} />
+        : <Button text="Sair" onClick={logout} />;
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            {button}
+        </div>
     );
 }
