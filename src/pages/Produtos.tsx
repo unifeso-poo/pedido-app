fe1d41da80f9c9b85e74545ff3200e8f0581a18d
Thales Azevedo <thales.azevedo@hotmail.com>
2021-09-22 01:07:39 -0300

    melhorando design

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Form, Navbar, FormControl, Button, Collapse, ListGroup, ButtonGroup } from "react-bootstrap";
+import { Container, Form, Navbar, FormControl, Button, Collapse, ListGroup, ButtonGroup, Row, Col } from "react-bootstrap";
 import Logo from "../logo.svg";
 import { useHistory } from "react-router-dom";
 import { useQuery } from "../hooks/useQuery";
@@ -121,101 +121,108 @@ export const Produtos: React.FC = () => {
           <Navbar.Brand onClick={() => history.goBack()}>
             <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="Pedidos logo" />
           </Navbar.Brand>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="mr-2"
-              aria-label="Search"
-              aria-expanded={isOpen}
-              onInput={(e) => {
-                filtroProdutoTemporario.nome = (e.target as HTMLInputElement).value;
-              }}
-            />
-            <Button variant="outline-success" onClick={() => setFiltroProduto(filtroProdutoTemporario)}>
-              Pesquisar
-            </Button>
-            <Button variant="outline-success" onClick={() => setIsOpen(!isOpen)} aria-controls="example-collapse-text" aria-expanded={isOpen}>
-              Filtros
-            </Button>
-          </Form>
-          <Container className="d-block align-itens-left">
-            <Collapse in={isOpen}>
-              <Form>
+        </Container>
+      </Navbar>
+      <Row className="h-100 ">
+        <Col className="d-flex justify-content-center col-4 bg-dark h-100">
+          <Container className="d-flex flex-column justify-content-end">
+            <Form className="d-flex m-1">
+              <Container>
                 <FormControl
                   type="search"
-                  placeholder="Id"
-                  className="mr-2 m-1 w-10"
+                  placeholder="Produto"
+                  className="m-1"
                   aria-label="Search"
-                  onInput={(e) => {
-                    filtroProdutoTemporario.id = parseInt((e.target as HTMLInputElement).value);
-                  }}
-                />
-                <FormControl
-                  type="search"
-                  placeholder="Nome do Produto"
-                  className="mr-2 m-1 w-10"
-                  aria-label="Search"
+                  aria-expanded={isOpen}
                   onInput={(e) => {
                     filtroProdutoTemporario.nome = (e.target as HTMLInputElement).value;
                   }}
                 />
-                <FormControl
-                  type="search"
-                  placeholder="Preço Mímino"
-                  className="mr-2 m-1 w-10"
-                  aria-label="Search"
-                  onInput={(e) => {
-                    filtroProdutoTemporario.precoMinimo = parseInt((e.target as HTMLInputElement).value);
-                  }}
-                />
-                <FormControl
-                  type="search"
-                  placeholder="Preço Máximo"
-                  className="mr-2 m-1 w-10"
-                  aria-label="Search"
-                  onInput={(e) => {
-                    filtroProdutoTemporario.precoMaximo = parseInt((e.target as HTMLInputElement).value);
-                  }}
-                />
-              </Form>
-            </Collapse>
+                <Collapse in={isOpen}>
+                  <Form className="">
+                    <FormControl
+                      type="search"
+                      placeholder="Id"
+                      className=" m-1 "
+                      aria-label="Produto"
+                      onInput={(e) => {
+                        filtroProdutoTemporario.id = parseInt((e.target as HTMLInputElement).value);
+                      }}
+                    />
+                    <FormControl
+                      type="search"
+                      placeholder="Preço Mímino"
+                      className=" m-1 "
+                      aria-label="Search"
+                      onInput={(e) => {
+                        filtroProdutoTemporario.precoMinimo = parseInt((e.target as HTMLInputElement).value);
+                      }}
+                    />
+                    <FormControl
+                      type="search"
+                      placeholder="Preço Máximo"
+                      className=" m-1"
+                      aria-label="Search"
+                      onInput={(e) => {
+                        filtroProdutoTemporario.precoMaximo = parseInt((e.target as HTMLInputElement).value);
+                      }}
+                    />
+                  </Form>
+                </Collapse>
+              </Container>
+
+              <Button variant="info" className="text-light text-bold  sm" onClick={() => setFiltroProduto(filtroProdutoTemporario)}>
+                Pesquisar
+              </Button>
+            </Form>
+
+            <Button
+              variant="outline-info"
+              className=" m-1"
+              onClick={() => setIsOpen(!isOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={isOpen}
+            >
+              Filtrar
+            </Button>
           </Container>
-        </Container>
-      </Navbar>
-      <Container className={" m-auto"}>
-        <ListGroup variant="flush" className="dark">
-          <ListGroup.Item className="d-flex justify-content-between m-1 rounded-3 bg-dark text-light">
-            <h5>{`ID`}</h5>
-            <h5>{`Produto`}</h5>
-            <h5>{`Preço`}</h5>
-            <h5>{`Quantidade`}</h5>
-            <div></div>
-          </ListGroup.Item>
-          {data
-            ?.filter((produto) => filtroProduto.comparaProduto(produto))
-            .map((produto) => {
-              console.log("passou no filtro");
-              console.log(produto);
-              return (
-                <ListGroup.Item className="d-flex justify-content-between text-center m-1 rounded-3">
-                  <p>{`${produto?.id}`}</p>
-                  <p>{`${produto?.nome}`}</p>
-                  <p>{`R$ ${produto?.preco / 100}`}</p>
-                  <p>{`${produto?.quantidadeDisponivel}`}</p>
-                  <ButtonGroup aria-label="Basic example">
-                    <Button className="" variant="success" size="sm">
-                      ...
-                    </Button>
-                    <Button className="-4" variant="danger" size="sm">
-                      X
-                    </Button>
-                  </ButtonGroup>
-                </ListGroup.Item>
-              );
-            })}
-        </ListGroup>
-      </Container>
+        </Col>
+        <Col className="d-flex flex-column justify-content-center col-8 w-66 m-auto">
+          <h3 className="m-auto mt-4">Lista de Produtos</h3>
+          <ListGroup variant="flush" className="dark w-100 p-3">
+            <ListGroup.Item className="d-flex justify-content-between m-1 rounded-3 bg-dark text-light">
+              <h5>{`ID`}</h5>
+              <h5>{`Produto`}</h5>
+              <h5>{`Preço`}</h5>
+              <h5>{`Quantidade`}</h5>
+              <div></div>
+            </ListGroup.Item>
+            {data
+              ?.filter((produto) => filtroProduto.comparaProduto(produto))
+              .map((produto) => {
+                console.log("passou no filtro");
+                console.log(produto);
+                return (
+                  <ListGroup.Item className="d-flex justify-content-between text-center m-1 rounded-3">
+                    <p>{`${produto?.id}`}</p>
+                    <p>{`${produto?.nome}`}</p>
+                    <p>{`R$ ${produto?.preco / 100}`}</p>
+                    <p>{`${produto?.quantidadeDisponivel}`}</p>
+                    <ButtonGroup aria-label="Basic example">
+                      <Button className="px-3 mx-1 text-light" variant="info" size="sm">
+                        ...
+                      </Button>
+                      <Button className="px-3" variant="outline-info" size="sm">
+                        X
+                      </Button>
+                    </ButtonGroup>
+                  </ListGroup.Item>
+                );
+              })}
+          </ListGroup>
+        </Col>
+      </Row>
+      <Container className={" m-auto"}></Container>
     </div>
   );
 };
