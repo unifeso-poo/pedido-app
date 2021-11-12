6edd3ada931ced50c35d95ab9532eade9fdef0fe
Romulo <romulotere@gmail.com>
2021-11-11 22:07:31 -0300

    ExercicioFlex

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Card } from "../components/Card";
 import { Cards } from "../components/Cards";
+import { FiUser, FiUsers} from "react-icons/fi";
+import { GoGraph} from "react-icons/go";
+import { BsBoxSeam} from "react-icons/bs";
+import { MdOutlineAttachMoney} from "react-icons/md"
+
 
 export const Home: React.FC = () => {
     return (
@@ -14,6 +19,24 @@ export const Home: React.FC = () => {
                     <Card title="Últimas atualizações" />
                     <Card title="Estoque baixo" />
                 </Cards>
+                <footer className ="tabbar">
+                    <button className="tabbar__button">
+                        <GoGraph/>
+                        <p className="tabbar__title">Inicio</p>
+                    </button>
+                    <button className="tabbar__button">
+                        <BsBoxSeam/>
+                        <p className="tabbar__title">Produtos</p>
+                    </button>
+                    <button className="tabbar__button">
+                        <FiUser/>
+                        <p className="tabbar__title">Clientes</p>
+                    </button>
+                    <button className="tabbar__button">
+                        <MdOutlineAttachMoney/>
+                        <p className="tabbar__title">Finanças</p>
+                    </button>                    
+                </footer>
             </div>
         </div>
     );
