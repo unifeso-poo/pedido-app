083a23137d0c3f03d5d401f79e166c542637ee9e
JulioHeroZ <johndoe@example.comjuliohero64@gmail.com>
2021-11-12 22:21:33 -0300

    Trabalho Dashboard (Julio e Joede)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Card } from "../components/Card";
 import { Cards } from "../components/Cards";
+import { FiUser, FiUsers} from "react-icons/fi";
+import { GoGraph} from "react-icons/go";
+import { BsBoxSeam} from "react-icons/bs";
+import { MdOutlineAttachMoney} from "react-icons/md"
+
 
 export const Home: React.FC = () => {
     return (
@@ -14,6 +19,24 @@ export const Home: React.FC = () => {
                     <Card title="Últimas atualizações" />
                     <Card title="Estoque baixo" />
                 </Cards>
+                <footer className ="tabbar">
+                    <button className="tabbarbutton">
+                        <GoGraph/>
+                        <p className="tabbartitle">Inicio</p>
+                    </button>
+                    <button className="tabbarbutton">
+                        <BsBoxSeam/>
+                        <p className="tabbartitle">Produtos</p>
+                    </button>
+                    <button className="tabbarbutton">
+                        <FiUser/>
+                        <p className="tabbartitle">Clientes</p>
+                    </button>
+                    <button className="tabbarbutton">
+                        <MdOutlineAttachMoney/>
+                        <p className="tabbartitle">Finanças</p>
+                    </button>
+                </footer>
             </div>
         </div>
     );
